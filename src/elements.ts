5fc67e9365489a62f00a725341fec6a39cea496d
osawa-koki <koki@osawa.dev>
2024-01-02 11:13:31 +0900

    要素の取得。

---
+++ src/elements.ts
@@ -0,0 +1,12 @@
+export const buttonElement = document.getElementById('button') as HTMLButtonElement
+
+export const executeCountInputElement = document.getElementById('execute-count-input') as HTMLInputElement
+export const executeCountShowElement = document.getElementById('execute-count-show') as HTMLSpanElement
+
+export const pointsInElement = document.getElementById('points-in') as HTMLSpanElement
+export const pointsOutElement = document.getElementById('points-out') as HTMLSpanElement
+
+export const ratioInElement = document.getElementById('ratio-in') as HTMLSpanElement
+export const ratioOutElement = document.getElementById('ratio-out') as HTMLSpanElement
+
+export const paiElement = document.getElementById('pai') as HTMLSpanElement
