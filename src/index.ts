74189502c7965bd610642516c43a51beb1bd8cbe
osawa-koki <koki@osawa.dev>
2024-01-02 11:36:45 +0900

    show

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { buttonElement, canvasSize, ctx } from './elements'
+import { buttonElement, canvasSize, ctx, paiElement, pointsInElement, pointsOutElement, ratioInElement, ratioOutElement } from './elements'
 
 const data = {
   running: false,
@@ -32,7 +32,20 @@ buttonElement.addEventListener('click', () => {
       ctx.fillStyle = isIn ? '#f00' : '#00f'
       ctx.fillRect(x, y, 1, 1)
       data[isIn ? 'in' : 'out']++
+      show()
       await new Promise(resolve => setTimeout(resolve, 10))
     }
   })()
 })
+
+function show (): void {
+  const { in: inPoints, out: outPoints } = data
+  const totalPoints = inPoints + outPoints
+  const ratio = inPoints / totalPoints
+  const pai = ratio * 4
+  pointsInElement.textContent = inPoints.toFixed(0)
+  pointsOutElement.textContent = outPoints.toFixed(0)
+  ratioInElement.textContent = (ratio * 100).toFixed(5)
+  ratioOutElement.textContent = ((1 - ratio) * 100).toFixed(5)
+  paiElement.textContent = pai.toFixed(10)
+}
